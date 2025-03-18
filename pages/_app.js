fc7b0697a150fe0bca2ce65178f188198fadd5fe
localnetwork <diomenikep@gmail.com>
2025-03-18 14:07:17 +0800

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,43 +1,20 @@
 import "@/styles/globals.css";
 import { useEffect, useMemo, useState } from "react";
-
-const detectBot = () => {
-  if (typeof window === "undefined") return false;
-
-  const userAgent = navigator.userAgent.toLowerCase();
-
-  const botPatterns = [
-    "bot",
-    "crawler",
-    "spider",
-    "headless",
-    "slurp",
-    "googlebot",
-    "google",
-    "Google",
-    "bingbot",
-    "yandex",
-    "baidu",
-  ];
-
-  alert(userAgent);
-  return botPatterns.some((pattern) => userAgent.includes(pattern));
-};
+import { isbot } from "isbot";
 
 const BotAlert = () => (
   <div className="bg-red-500 text-white p-2">Bot detected!</div>
 );
 
 export default function App({ Component, pageProps }) {
-  const [isBot, setIsBot] = useState(false);
+  const [bot, setBot] = useState(false);
 
   useEffect(() => {
-    setIsBot(detectBot());
+    setBot(isbot(navigator.userAgent));
   }, []);
-
   return (
     <div>
-      {isBot && <BotAlert />}
+      {bot ? <BotAlert /> : <div>Not Bot</div>}
       <Component {...pageProps} />
     </div>
   );
